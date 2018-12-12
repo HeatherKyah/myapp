75f3e08deb3c3aeb89f5a62a3fc4696eb88ff59c
Heather Kerrigan <hkerri200@gmail.com>
2018-12-12 12:05:08 +0000

    ok

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('./config/database');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+
+
+try{
+  mongoose.connect(config.database);
+}
+catch(exception e){
+  console.log(e.message);
+}
 
 var app = express();
+
+
+//sert up cpours
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(passport.initialize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+//set up routes
+var api = require('./routes/api');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +47,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req, res) {
+  res.send('Page under construction.');
+});
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
